38398de2e1840f11cd11f39c68840870b2aa07a4
omnia <omniaabdellatef193@gmail.com>
2023-07-03 23:49:31 +0200

    crud

--- BackEnd/Router/CURD operation.js
+++ BackEnd/Router/CURD operation.js
@@ -55,62 +55,62 @@ student.get('/studentdetails/:id',
 
 
 
-// student.put('/studentupdate/:id',
-//     body('name').notEmpty().withMessage('Name is required'),
-//     body('password').notEmpty().withMessage('Password is required'),
-//     body('national_id').isInt().withMessage('nationalId is required'),
-//     body('email').isEmail().withMessage('Not a valid e-mail address'),
-//     body('phone').isInt().withMessage('phone is required'),
-//     body('gender').notEmpty().withMessage('gentder is required'),
-//     body("level").notEmpty().withMessage('Educational_level is required'),
-//     body('dateOfBirth').notEmpty().withMessage('dateOfBirth is required'),
-//     body('military_status').notEmpty().withMessage('military_status is required'),
+student.put('/studentupdate/:id',
+    body('name').notEmpty().withMessage('Name is required'),
+    body('password').notEmpty().withMessage('Password is required'),
+    body('national_id').isInt().withMessage('nationalId is required'),
+    body('email').isEmail().withMessage('Not a valid e-mail address'),
+    body('phone').isInt().withMessage('phone is required'),
+    body('gender').notEmpty().withMessage('gentder is required'),
+    body("level").notEmpty().withMessage('Educational_level is required'),
+    body('dateOfBirth').notEmpty().withMessage('dateOfBirth is required'),
+    body('military_status').notEmpty().withMessage('military_status is required'),
 
 
-//     async (req, res) => {
-//         try {
-//             const errors = validationResult(req);
-//             if (!errors.isEmpty()) {
-//                 return res.status(400).json({ errors: errors.array() });
-//             }
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
 
-//             const sqlcheck = "SELECT * FROM students WHERE student_id = ?";
-//             const value = [req.params.id];
+            const sqlcheck = "SELECT * FROM students WHERE student_id = ?";
+            const value = [req.params.id];
 
-//             const studentdetails = await query(sqlcheck, value);
+            const studentdetails = await query(sqlcheck, value);
 
-//             if (!studentdetails[0]) {
-//                 return res.status(400).json({ msg: "Error: student Not Found!" });
-//             }
+            if (!studentdetails[0]) {
+                return res.status(400).json({ msg: "Error: student Not Found!" });
+            }
 
-//             if (req.body.email != studentdetails[0].email) {
-//                 return res.status(400).json({ errors: [{ msg: "Email Is Not Allow to Chage !" }] });
-//             }
+            if (req.body.email != studentdetails[0].email) {
+                return res.status(400).json({ errors: [{ msg: "Email Is Not Allow to Chage !" }] });
+            }
 
-//             if (req.body.password) {
-//                 const pass = await bcrypt.hash(req.body.password, 10);
-//                 const sqlUpdate = "UPDATE students SET student_name = ?, email = ?, phonenumber = ?, password = ? WHERE student_id = ?";
-//                 const values = [req.body.student_name, req.body.email, req.body.phonenumber, pass, req.params.id];
-//                 return await query(sqlUpdate, values, (err) => {
+            if (req.body.password) {
+                const pass = await bcrypt.hash(req.body.password, 10);
+                const sqlUpdate = "UPDATE students SET student_name = ?, email = ?, phonenumber = ?, password = ? WHERE student_id = ?";
+                const values = [req.body.student_name, req.body.email, req.body.phonenumber, pass, req.params.id];
+                return await query(sqlUpdate, values, (err) => {
 
-//                      if (err) {
-//                             console.log(err);
-//                         }
-//                      res.status(200).json({ msg: "Student Updated Successfully" });
-//                 });
-//             } else {
+                     if (err) {
+                            console.log(err);
+                        }
+                     res.status(200).json({ msg: "Student Updated Successfully" });
+                });
+            } else {
 
-//             const sqlUpdate = "UPDATE students SET ?  WHERE student_id = ?";
-//             const values = [studentData, req.params.id];
-//             await query(sqlUpdate, values);
+            const sqlUpdate = "UPDATE students SET ?  WHERE student_id = ?";
+            const values = [studentData, req.params.id];
+            await query(sqlUpdate, values);
 
-//             res.status(200).json({ msg: "Student Updated Successfully" });
-//             }
-//         } catch (err) {
-//             console.log(err);
-//             res.status(500).json({ msg: "Server Error" });
-//         }
-//     });
+            res.status(200).json({ msg: "Student Updated Successfully" });
+            }
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ msg: "Server Error" });
+        }
+    });
 
 
 
